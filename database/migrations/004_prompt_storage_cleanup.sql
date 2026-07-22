c7e09a3ebbaeb63549e6dfad6d583366b265e6a1
Arthur Tkachenko <arturtkaczenko@gmail.com>
2026-07-22 22:16:27 +0300

    Remove legacy schema migrations

--- database/migrations/004_prompt_storage_cleanup.sql
+++
@@ -1,7 +0,0 @@
-DO $$
-DECLARE
-    remove_action TEXT := chr(68) || chr(82) || chr(79) || chr(80);
-BEGIN
-    EXECUTE 'ALTER TABLE ai_system_prompts ' || remove_action || ' COLUMN IF EXISTS description';
-    EXECUTE 'ALTER TABLE ai_system_prompt_versions ' || remove_action || ' COLUMN IF EXISTS description';
-END $$;
