08e03c7d530c5da813bda8a008082fcc98788d94
Arthur Tkachenko <arturtkaczenko@gmail.com>
2026-07-24 14:53:18 +0300

    Register Hook management UI

--- routes.py
+++ routes.py
@@ -8,11 +8,13 @@ from api.prompt_compilation import router as prompt_compilation_router
 from api.prompt_management import router as prompt_management_router
 from db import db_health_check
 from ui.deleted_records import router as deleted_records_ui_router
+from ui.hook_management import router as hook_management_ui_router
 from ui.prompt_management import router as prompt_management_ui_router
 
 
 router = APIRouter()
 router.include_router(prompt_management_ui_router)
+router.include_router(hook_management_ui_router)
 router.include_router(deleted_records_ui_router)
 router.include_router(prompt_management_router)
 router.include_router(hook_management_router)
